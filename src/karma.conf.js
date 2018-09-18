4811bb605a5345a9758abc08d2d6b8ebbea02e2f
lovo-h <lovohh@gmail.com>
2018-09-18 01:18:49 -0400

    Replacing Linter: JS- For TS-Linter
    Since we're using TypeScript

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,121 +4,134 @@ const path = require('path');
 
 
 module.exports = function (config) {
-    config.set({
+  config.set({
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'browserify'],
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'browserify'],
 
-        plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('karma-browserify')
-        ],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-browserify'),
+    ],
 
-        client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    // list of files / patterns to load in the browser
+    files: [
+      '**/*.spec.js',
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      '**/*.js': ['browserify'],
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
+    // reporters: ['coverage-istanbul'],
+
+    // any of these options are valid:
+    // https://github.com/istanbuljs/istanbuljs/blob/
+    // aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+
+    coverageIstanbulReporter: {
+      // reports can be any that are listed here:
+      // https://github.com/istanbuljs/istanbuljs/tree/
+      // aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+      reports: ['html', 'lcovonly'],
+
+      // base output directory. If you include %browser% in the path it will be
+      // replaced with the karma browser name
+      dir: path.join(__dirname, 'coverage'),
+
+      // Combines coverage information from multiple browsers into one report
+      // rather than outputting a report
+      // for each browser.
+      // As of now, this isn't really needed since only Chrome is being used.
+      combineBrowserReports: true,
+
+      // Omit files with no statements, no functions and no branches from the
+      // report
+      skipFilesWithNoCoverage: true,
+
+      // Most reporters accept additional config options. You can pass these
+      // through the `report-config` option
+      'report-config': {
+        // all options available at: https://github.com/istanbuljs/istanbuljs/blob/
+        // aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/
+        // lib/html/index.js#L135-L137
+        html: {
+          // outputs the report in ./coverage/html
+          subdir: 'html',
         },
-        // list of files / patterns to load in the browser
-        files: [
-            '**/*.spec.js'
-        ],
+      },
 
-
-        // list of files / patterns to exclude
-        exclude: [
-        ],
-
-
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
-            '**/*.js': ['browserify']
+      // enforce percentage thresholds
+      // anything under these percentages will cause karma to fail with an exit
+      // code of 1 if not running in watch mode
+      thresholds: {
+        // set to `true` to not fail the test command when thresholds are not met
+        emitWarning: false,
+        // thresholds for all files
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
         },
+      },
+
+      // verbose: true // output config used by istanbul for debugging
+    },
 
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
-        // reporters: ['coverage-istanbul'],
-
-        // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
-        coverageIstanbulReporter: {
-            // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
-            reports: ['html', 'lcovonly'],
-
-            // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
-            dir: path.join(__dirname, 'coverage'),
-
-            // Combines coverage information from multiple browsers into one report rather than outputting a report
-            // for each browser.
-            // As of now, this isn't really needed since only Chrome is being used.
-            combineBrowserReports: true,
-
-            // Omit files with no statements, no functions and no branches from the report
-            skipFilesWithNoCoverage: true,
-
-            // Most reporters accept additional config options. You can pass these through the `report-config` option
-            'report-config': {
-                // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
-                html: {
-                    // outputs the report in ./coverage/html
-                    subdir: 'html'
-                }
-            },
-
-            // enforce percentage thresholds
-            // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
-            thresholds: {
-                emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
-                // thresholds for all files
-                global: {
-                    statements: 100,
-                    lines: 100,
-                    branches: 100,
-                    functions: 100
-                }
-            },
-
-            // verbose: true // output config used by istanbul for debugging
-        },
+    // web server port
+    port: 9876,
 
 
-        // web server port
-        port: 9876,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
 
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
 
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
-
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: Infinity,
-        verbose: true
-    })
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+    verbose: true,
+  });
 };
