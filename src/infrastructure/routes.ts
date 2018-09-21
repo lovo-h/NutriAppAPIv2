e004085968c9d82494fcb435ebffb8b7715f498c
lovo-h <lovohh@gmail.com>
2018-09-20 23:40:57 -0400

    Changing Route: '/api/long_descs/:snippet'
    Instead of an ampersand (&) representing white-space,
    make underscore (_) represent white-space.

--- src/infrastructure/routes.ts
+++ src/infrastructure/routes.ts
@@ -2,7 +2,7 @@ import { Express, Request, Response } from 'express';
 import { HandlerWebservice } from '../interfaces/handler.webservice';
 
 export function initialize(app: Express, ws: HandlerWebservice) {
-  app.get('/api/long_descs/:snippet([a-zA-Z0-9&]{3,200})', (req: Request, res: Response) => {
+  app.get('/api/long_descs/:snippet([a-zA-Z0-9_]{3,200})', (req: Request, res: Response) => {
     ws.longDescs(req, res);
   });
 
